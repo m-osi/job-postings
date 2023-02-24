9e0f4897f0e4d825d45ebf51d7cc30e741f146ac
Marta Osińska <martasnsk@gmail.com>
2023-02-24 01:56:49 +0100

    🔧 all config files

---
+++ config/config.py
@@ -0,0 +1,8 @@
+SOURCE_URL = 'https://justjoin.it/api/offers'
+
+QUERY_PARAMS = {
+    'marker_icon': ['data'],
+    'experience_level': ['junior', 'mid'],
+    'city': ['Warszawa'],
+    'workplace_type': ['partly_remote', 'office']
+}
