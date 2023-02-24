4e4ba8a507a0ba30bd7070dcc0ad7de26925ab2f
Marta Osińska <martasnsk@gmail.com>
2023-02-24 16:22:39 +0100

    🚧 wip expanding employment_types structure

--- etl/transform.py
+++ etl/transform.py
@@ -3,8 +3,6 @@ from etl.extract import PostingsExtractor
 import pandas as pd
 import json
 
-Extractor = PostingsExtractor(SOURCE_URL)
-data = Extractor.extract()
 
 class PostingsTransformer:
     def __init__(self, params):
@@ -15,14 +13,24 @@ class PostingsTransformer:
         data = self.parse_to_json(content)
 
         df = pd.DataFrame(data)
-        df = df[df.set_index(list(self.params.keys()))
-                .index.isin(zip(*list(self.params.values())))]
-        
+
+        # df = df[df.set_index(list(self.params.keys()))
+        #         .index.isin(zip(*list(self.params.values())))]
+
         return df
-    
+
     @staticmethod
     def parse_to_json(content):
         return (json.loads(content))
 
+    @staticmethod
+    def expand_employment_types(df):
+        df.explode('employment_types').reset_index(inplace=True)
+        # to concatenate do poprzedniej
+        pd.json_normalize(df['employment_types'])
 
 
+Extractor = PostingsExtractor(SOURCE_URL)
+data = Extractor.extract()
+Transformer = PostingsTransformer(QUERY_PARAMS)
+df = Transformer.transform(data)
