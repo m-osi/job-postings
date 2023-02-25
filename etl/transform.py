81b293191a11025ddc37d8ddccf872900e623dbc
Marta Osińska <martasnsk@gmail.com>
2023-02-25 01:10:39 +0100

    ✨ expand columns with lists

--- etl/transform.py
+++ etl/transform.py
@@ -1,19 +1,20 @@
-from config.config import SOURCE_URL, QUERY_PARAMS
-from etl.extract import PostingsExtractor
 import pandas as pd
 import json
 
 
 class PostingsTransformer:
-    def __init__(self, params):
-        self.params = params
 
     def transform(self, content):
 
         data = self.parse_to_json(content)
-
         df = pd.DataFrame(data)
-
+        df = self.expand_columns(df, ['employment_types', 'skills'])
+        df.drop(columns=['id', 'company_logo_url',
+                'salary', 'multilocation'], inplace=True)
+        df.rename({'salary.to': 'salary_to',
+                   'salary.from': 'salary_from',
+                   'salary.currency': 'salary_currency'}, axis=1, inplace=True)
+        df.drop_duplicates(inplace=True)
         # df = df[df.set_index(list(self.params.keys()))
         #         .index.isin(zip(*list(self.params.values())))]
 
@@ -24,13 +25,11 @@ class PostingsTransformer:
         return (json.loads(content))
 
     @staticmethod
-    def expand_employment_types(df):
-        df.explode('employment_types').reset_index(inplace=True)
-        # to concatenate do poprzedniej
-        pd.json_normalize(df['employment_types'])
-
-
-Extractor = PostingsExtractor(SOURCE_URL)
-data = Extractor.extract()
-Transformer = PostingsTransformer(QUERY_PARAMS)
-df = Transformer.transform(data)
+    def expand_columns(df, columns):
+        for column in columns:
+            df = df.explode(column).reset_index()
+        df.drop(columns=['level_0', 'index'], inplace=True)
+        for column in columns:
+            df = pd.concat([df, pd.json_normalize(df[column])], axis=1)
+            df.drop(columns=column, inplace=True)
+        return df
