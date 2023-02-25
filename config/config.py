e2439ee07d2ea816d5903df93e989f502f7dc3f5
Marta Osińska <martasnsk@gmail.com>
2023-02-25 01:11:39 +0100

    🔧 added connection string

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
+import os
+
 SOURCE_URL = 'https://justjoin.it/api/offers'
 
 QUERY_PARAMS = {
@@ -6,3 +8,8 @@ QUERY_PARAMS = {
     'city': ['Warszawa'],
     'workplace_type': ['partly_remote', 'office']
 }
+
+CONNECTION_STRING = f"postgresql://postgres:{os.environ['POSTGRES_PASSWORD']}@localhost:5432/{os.environ['POSTGRES_DB']}"
+
+#docker build -t my-postgres-db --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD --build-arg POSTGRES_DB=$POSTGRES_DB .
+#docker run -d --name my-postgresdb-container -p 5432:5432 my-postgres-db          
